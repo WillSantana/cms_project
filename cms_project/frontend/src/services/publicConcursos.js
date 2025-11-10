163ed1b178fe32e74e0202389c7ba97af009969a
Wilson Santana <willjsantana@gmail.com>
2025-11-09 21:19:59 -0300

    Adiciona submódulo appconcursofront

--- cms_project/frontend/src/services/publicConcursos.js
+++
@@ -1,7 +0,0 @@
-// src/services/publicConcursos.js
-import api from "./api";
-
-export async function getConcursosPublic() {
-  const res = await api.get("/public/concursos/");
-  return res.data;
-}
